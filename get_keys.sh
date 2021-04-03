af8c567d79cc59adaa0570a5c371c80d3a7f1bb7
Álvaro Rodríguez Bonmatí <43890020+arodriguezbonmati@users.noreply.github.com>
2021-04-03 11:39:30 +0200

    Update get_keys.sh

--- get_keys.sh
+++ get_keys.sh
@@ -1,12 +1,15 @@
-# Get public key & node's address
+#!/bin/bash
+
+# Get public key
 /data/besu/bin/besu public-key export --to=/data/besu/keys/besu/key.pub
 
+# Get node's address
 /data/besu/bin/besu public-key export-address --to=/data/besu/keys/besu/nodeAddress
 
 # Move new private key to the keys directory
 mv /data/besu/key /data/besu/keys/besu/key
 
-# Get enode and move it to the keys directory.
+# Build enode and move it to the keys directory.
 pubkey=$(cat /data/besu/keys/besu/key.pub)
 pubkey="${pubkey:2}"
 ip=$(ifconfig enp0s3 | grep "inet " | awk '{print $2}')
